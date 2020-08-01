0f93ae4d766037c0be8936d4d0c595a012e5bcea
Santiago Casa <santiagoacasa@gmail.com>
2020-08-01 12:01:41 +0200

    Navbar como CustomNavBar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage';
-import Navbar from './components/Navbar';
+import CustomNavbar from './components/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <CustomNavbar />
       <Switch>
         <Route exact path="/" render={(props) => <Homepage {...props} />} />
       </Switch>
