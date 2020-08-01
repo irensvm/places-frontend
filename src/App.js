c3e755bd56034bcf112161286c79d7c8e9ee0e22
JesusSantana <jesus@esmarketingdigital.com>
2020-08-01 11:51:50 +0200

    Add Place update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import AddPlace from './components/AddPlace';
+//import AddPlace from './components/AddPlace';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <AddPlace/>
+      
       <Switch>
         <Route exact path="/" render={(props) => <Homepage {...props} />} />
       </Switch>
