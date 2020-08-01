ba5459596a7b72e14612d67f44425031e47f09e8
Santiago Casa <santiagoacasa@gmail.com>
2020-08-01 12:49:07 +0200

    imagenes en la homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage';
-import Navbar from './components/Navbar';
 import EditPlace from './components/EditPlace';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import PlacesList from './components/PlacesList';
 import CustomNavbar from './components/Navbar';
 import Search from './components/Search';
+import AddPlace from './components/AddPlace';
 
 
 function App() {
@@ -19,6 +19,7 @@ function App() {
         <Route exact path='/edit/:id' render={(props) => <EditPlace {...props} />} />
         <Route exact path="/list" render={(props) => <PlacesList {...props} />} />
         <Route exact path="/search" render={(props) => <Search {...props} />} />
+        <Route exact path="/addplace" render={(props) => <AddPlace {...props} />} />
       </Switch>
     </div>
   );
